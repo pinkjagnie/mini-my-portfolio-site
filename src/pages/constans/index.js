b3f979a994cb0379cba5f67a0a3367e8896a29da
pinkjagnie <olga.krzysztofik@gmail.com>
2023-06-30 20:36:01 +0200

    Added details about links

---
+++ src/pages/constans/index.js
@@ -0,0 +1,38 @@
+export const contentLinks = [
+  {
+    id: '1',
+    icon: '👩🏻‍💻',
+    title: 'poznaj społeczność kobiet w IT',
+    link: 'https://www.pinkjagnie.pl/spolecznosc-kobiet-w-it'
+  },
+  {
+    id: '2',
+    icon: '🐾',
+    title: 'zobacz najsłodszego psa w branży IT',
+    link: 'https://www.instagram.com/pixel.rad/'
+  },
+  {
+    id: '3',
+    icon: '🚀',
+    title: 'przejdź na moją stronę',
+    link: 'https://www.pinkjagnie.pl/'
+  },
+  {
+    id: '4',
+    icon: '📧',
+    title: 'napisz do mnie maila',
+    link: 'mailto:olga.krzysztofik@gmail.com'
+  },
+  {
+    id: '5',
+    icon: '💡',
+    title: 'bądźmy w kontakcie na LinkedInie',
+    link: 'https://www.linkedin.com/in/olga-krzysztofik/'
+  },
+  {
+    id: '6',
+    icon: '👑',
+    title: 'zobacz biżuterię tworzoną przez rękodzielnika - moją Mamę',
+    link: 'https://www.facebook.com/pracownia.rekodziela.ka'
+  },
+];
