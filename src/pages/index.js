f22462936d289e55a358e013531b190322a1cd0c
pinkjagnie <olga.krzysztofik@gmail.com>
2023-06-30 19:30:46 +0200

    Added Hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Inter } from '@next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+import Hero from './components/Hero';
+
 export default function Home() {
   return (
     <>
@@ -12,8 +14,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/myFavicon_48-48_square.png" />
       </Head>
-      <main>
-        <p>Hi</p> 
+      <main className='min-h-screen bg-pink-100'>
+        <Hero /> 
       </main>
     </>
   )
